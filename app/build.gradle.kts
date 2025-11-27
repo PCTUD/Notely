4d653c0c37c421f89afcce339f61aaef12d3c260
Paul <paul.colclough@sap.com>
2025-11-27 22:36:23 +0000

    kt Skeleton

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("kotlin-kapt")
 }
 
 android {
@@ -41,6 +42,13 @@ android {
 
 dependencies {
 
+    //ROOM
+    val roomVersion = '2.6.1'
+    implementation("androidx.room:room-runtime:$roomVersion")
+    kapt("androidx.room:room-compiler:$roomVersion")
+    implementation("androidx.room:room-ktx:$roomVersion")
+
+    //The rest
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,18 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
 }
+
+// No specific / directly copied lines but this page was informed by https://developer.android.com/training/data-storage/room
+
+dependencies {
+    def room_version = "2.6.1"
+
+    implementation "androidx.room:room-runtime:$room_version"
+    annotationProcessor "androidx.room:room-compiler:$room_version"
+
+    kapt "androidx.room:room-compiler:$room_version" //Kotlin
+    implementation "androidx.room:room-ktx:$room_version" //Livedata
+}
+
+// END material from above link
+
